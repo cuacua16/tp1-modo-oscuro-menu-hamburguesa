a7e5490a8e1703d77681ec2ea51c690f8828cc5a
cuacua16 <agborrelli@hotmail.com>
2022-07-27 17:30:08 -0300

    commit 1

---
+++ scripts.js
@@ -0,0 +1,38 @@
+let themeOn = false;
+const cambiarTema = () => {
+  if (!themeOn) {
+    document.querySelector(":root").style.setProperty("--primario2", "#444");
+    document.querySelector(":root").style.setProperty("--colorA", "#fdbb2e");
+    document.querySelector(":root").style.setProperty("--colorAA", "#eee");
+    document.querySelector(":root").style.setProperty("--colorAAA", "#ddd");
+    document.querySelector(":root").style.setProperty("--colorAAAA", "#aaa");
+    document.querySelector(":root").style.setProperty("--colorB", "#333");
+    document.querySelector(":root").style.setProperty("--colorBB", "#555");
+    document.querySelector(":root").style.setProperty("--colorBBB", "#666");
+
+    themeOn = true;
+  } else {
+    document.querySelector(":root").style.setProperty("--primario2", "#fdbb2e");
+    document.querySelector(":root").style.setProperty("--colorA", "#333");
+    document.querySelector(":root").style.setProperty("--colorAA", "#555");
+    document.querySelector(":root").style.setProperty("--colorAAA", "#666");
+    document.querySelector(":root").style.setProperty("--colorAAAA", "#999");
+    document.querySelector(":root").style.setProperty("--colorB", "#fff");
+    document.querySelector(":root").style.setProperty("--colorBB", "#eee");
+    document.querySelector(":root").style.setProperty("--colorBBB", "#ddd");
+    themeOn = false;
+  }
+  theme.classList.toggle("fa-sun");
+  theme.classList.toggle("fa-moon");
+};
+
+const theme = document.querySelector(".theme");
+
+theme.addEventListener("click", cambiarTema);
+
+const hambur = document.querySelector(".fa-bars");
+const navUl = document.getElementById("navUl");
+
+hambur.addEventListener("click", () => {
+  navUl.classList.toggle("displayOff");
+});
